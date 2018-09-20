74a7eca33913ce4674073d24ebcd62bfe9f4e4ae
agnipanda <pandaagni@outlook.com>
2018-09-20 07:25:15 +0530

    updated layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ChatboxComponent } from './chatbox/chatbox.component';
+import { LoginComponent } from './login/login.component';
+import { RouterModule } from '@angular/router';
+import { appRoutes } from '../routes';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatboxComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/chatbox/chatbox.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chatbox',
+  templateUrl: './chatbox.component.html',
+  styleUrls: ['./chatbox.component.css']
+})
+export class ChatboxComponent implements OnInit {
+
+  title = "Chatbox";
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+
+
+}
