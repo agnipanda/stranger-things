99c7200f137423decad16dcfa61cd327a540f923
agnipanda <pandaagni@outlook.com>
2018-09-25 22:51:37 +0530

    changed socket port to 3001.

--- public/javascripts/chatbox.js
+++ public/javascripts/chatbox.js
@@ -1,6 +1,30 @@
-var socket = io();
-socket.on('message', function(msg){
-      console.log(msg);
-      document.getElementById("display").innerHTML = msg;
-    });
+var socket = io("http://localhost:3001");
+
+$("#input").on("keypress",function(e) {
+    var key = e.keyCode;
+
+    // If the user has pressed enter
+    if (key == 13) {
+        send();
+        document.getElementById("input").value = "";
+        return false;
+    }
+    else {
+        return true;
+    }
+});
+
+function send() {
+    let m = document.getElementById('input').value;
+    document.getElementById('display').innerHTML += '<p>'+m+'</p>'
+    let z = 'a';
+    socket.emit('message',"user1"+m);
+    socket.on('message', function(data){
+        if(data.slice(0,5) === "user2"){
+            document.getElementById('display').innerHTML += '<p style="align:right">'+data.slice(5,data.length)+'</p>'
+            console.log(data);
+        }
+    })
+}
+
 console.log(socket);
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
 
+var io = require('socket.io').listen(3001, {log: false});
 /* GET home page. */
 router.get('/', function(req, res) {
   res.redirect('home');
@@ -14,17 +12,31 @@ router.get('/home', function(req, res) {
   res.render('login');
 });
 
-router.get('/chatbox', function(req, res) {
+var usermale = []
+var mp = -1
+var userfemale = []
+var fp = -1
 
-  console.log('emitted');
+router.get('/chatbox', function(req, res) {
+  io.on('connection', function(socket) {
+      console.log('A User Connected');
+      socket.on('message', function(data){
+          console.log(data);
+         if(data.slice(0,5) === 'user1'){
+             socket.send("user2"+(data).slice(5,data.length));
+         }
+         if(data.slice(0,5) === 'user2'){
+             socket.send("user1"+data.slice(5,data.length));
+         }
+      });
+   });
+
+
   res.render('chatbox', {'title': 'Chatbox'});
 });
 
-
-
 router.post('/home', function(req, res) {
   console.log(req.body);
-  io.emit('message', 11);
   res.redirect('chatbox');
 });
 
